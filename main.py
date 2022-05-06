1d444d86e88f3bcfed94aadf1075f5f5065a2e85
abduqodirbozorov <abduqodirnote@gmail.com>
2022-05-06 10:57:48 +0500

    Command haldler and some initial commits

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
+import json
+
 from telegram.ext import Updater, CommandHandler
 
 
+user = {
+    "name": "Qodir",
+    'origin': 'Najot talim',
+    "phone": '+998990545606',
+    "yunalishlar": [{'nomi': "XTA", 'otm': 'inyaz', 'mvdir': "55685425"}, {'nomi': "XTA", 'otm': 'inyaz', 'mvdir': "55685425"}, {'nomi': "XTA", 'otm': 'inyaz', 'mvdir': "55685425"}, {'nomi': "XTA", 'otm': 'inyaz', 'mvdir': "55685425"}, {'nomi': "XTA", 'otm': 'inyaz', 'mvdir': "55685425"}],
+    'id': '0008'
+}
+
+samdtu = {}
+
+
 def start(update, context):
-    update.message.reply_text("Salom")
+    update.message.reply_text(F"{samdtu['yunalishlar']}")
+
+
+with open('vuz-data.json') as json_file:
+    data = json.load(json_file)
+
+    samdtu = data[0]
+    # for reading nested data [0] represents
+    # the index value of the list
+
+    # for printing the key-value pair of
+    # nested dictionary for loop can be used
 
 
 def main():
